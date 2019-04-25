260dcfe7070344f06d973cad3ed6d29e5534a201
Patrick Candlin <pctennislessons@gmail.com>
2019-04-25 02:22:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object,{[key]: value});
+} 
+var recipes = {meal: "ingredients"};
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var obj = Object.assign({}, object);
+  var newObj = Object.assign({}, obj);
+  delete newObj.key;
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+  delete object.key;
+  return opject;
+} 
